b929e311ed16e775e848db67fe01f6c68e90970e
TungYuChen <77762947+TungYuChen@users.noreply.github.com>
2021-05-07 16:05:53 -0700

    Update longestPalindrome_1.java

--- longestPalindrome_1.java
+++ longestPalindrome_1.java
@@ -6,6 +6,9 @@ class Solution {
         int count = 1;
         for (int i = 0; i < ch.length; i++) {
             for (int j = ch.length -1; j > i; j--) {
+                if (j - i + 1 <= count) {
+                    break;
+                }
                 int start = i;
                 int end = j;
                 while (ch[start] == ch[end]) {
